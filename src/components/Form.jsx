fd9358b56af3d7d272346740d3d90fc2846fdb7f
MehrXloop <mehrunissa.pirwani@xloopdigital.com>
2023-04-13 23:20:38 +0500

    adding bootstrap

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Form() {
-    const [title,setTitle] = useState('');
-    const [des, setDes] = useState('')
-    function fSetTitle (e){
-        setTitle(e.value);
-    } function fSetDes(e){
-        setDes(e.value);
-    }
+  const [title, setTitle] = useState("");
+  const [des, setDes] = useState("");
 
-    return (
-        <div>
-            <h1>Log Your Goals For This Bootcamp</h1>
-            <div>
-                <label htmlFor="title">Title</label>
-                <input onChange={(e)=> fSetTitle(e)} value={title} type="text"  id='title'/>
-            </div>
-            <div>
-                <label htmlFor="des">Description</label>
-                <input type="text"  id='des' onChange={(e)=> fSetDes(e)} value={des}/>
-            </div>
-            <button onClick={fSave()}>Save</button>
+  function fSetTitle(e) {
+    setTitle(e.value);
+  }
+  function fSetDes(e) {
+    setDes(e.value);
+  }
+
+  return (
+    <>
+      {/* <div> */}
+      <h1>Log Your Goals For This Bootcamp</h1>
+      {/* <div>
+            <label htmlFor="title">Title</label>
+            <input onChange={(e)=> fSetTitle(e)} value={title} type="text"  id='title'/>
         </div>
-    )
+        <div>
+            <label htmlFor="des">Description</label>
+            <input type="text"  id='des' onChange={(e)=> fSetDes(e)} value={des}/>
+        </div>
+        <button>Save</button>
+    </div> */}
+
+      <div className="container">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Goal Title
+          </label>
+          <input
+            onChange={(e) => fSetTitle(e)}
+            value={title}
+            type="test"
+            class="form-control"
+            id="exampleFormControlInput1"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Goal Description
+          </label>
+          <textarea
+            onChange={(e) => fSetDes(e)}
+            value={des}
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="button" class="btn btn-primary">
+          Save
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Form
+export default Form;
--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function GoalList(props) {
   return (
     <div>
-        <h2>{props.title}</h2>
-        <p>{props.des}</p>
+      {/* <h2>{props.title}</h2>
+        <p>{props.des}</p> */}
+        <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">{props.title}</h5>
+    <p class="card-text">{props.des}</p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+</div>
     </div>
-  )
+  );
 }
 
-export default GoalList
+export default GoalList;
